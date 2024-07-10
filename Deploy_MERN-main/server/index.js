fa94408ccb1b88e4725ff9205fecc7500877834d
sethumadhavan7 <chandrabosechandrabose9@gmail.com>
2024-07-10 12:49:16 +0530

    Update index.js

--- Deploy_MERN-main/server/index.js
+++ Deploy_MERN-main/server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://sethumadhavan:indrajit@cluster0.dbntwx8.mongodb.net/mern-app?retryWrites=true&w=majority&appName=Cluster0');
 
 
 app.get("/", (req, res) => {
